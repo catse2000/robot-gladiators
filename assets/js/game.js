cca12fc6070a2e1cef123df65a4fd66edb0dcab6
catse2000 <catse2000@yahoo.com>
2020-08-15 17:12:03 -0500

    add array to add multiple robots as a feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,17 +2,15 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
-
-console.log(playerName, playerAttack, playerHealth);
-
-//Enemy robots information
-var enemyName = "Roborto";
-var enemyHealth = 50;
-var enemyAttack = 12;
 var playerMoney = 10;
 
+//Enemy robots information
+var enemyNames = ["Roborto","Amy Android", "Robo Trumble"];
+var enemyHealth = 50;
+var enemyAttack = 12;
+
 // this creates a function named "fight"
-var fight = function fight() {
+var fight = function (enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -65,7 +63,7 @@ var fight = function fight() {
 
         // if no (false) ask question again by running fight() again
         else{
-            fight();
+            fight();cd
         }
 
 
@@ -75,4 +73,6 @@ var fight = function fight() {
    
 }
 
-fight();
+for (var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
